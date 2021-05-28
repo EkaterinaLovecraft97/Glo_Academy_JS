8a33973113453e3bf3190f12d1dc60837bbdbfb7
Ekaterina Prisekkina <ekaterina.lovecraft.com@gmail.com>
2021-05-28 10:55:43 +0700

    Первый коммит

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let money = '1000';
+console.log('money: ', money);
+
+
+let income = '1000 dollars';
+alert(income);
+
+let addExpenses = (1000 + 500 + 102);
+console.log('addExpenses: ', addExpenses);
+
+let deposit = '50000';
+console.log('deposit: ', deposit);
+
+let mission = 'layout';
+alert(mission);
+
+let period = '2020-2021';
+console.log('period: ', period);
